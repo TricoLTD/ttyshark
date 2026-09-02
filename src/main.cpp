05cc048a7ee5d480c578824dce99224eda0d9668
ViridianAStar <bowman9594@gmail.com>
2026-09-02 11:15:26 -0400

    do some updates to the project and set some stuff up

--- src/main.cpp
+++ src/main.cpp
@@ -1,141 +1,157 @@
-#include "ttyshark.h"
-#include <vector>
-#include <string>
-
 #include "modbusRTU/modbusrtu.h"
 #include "serialcap/serialcap.h"
+#include "ttyshark.h"
 #include <chrono>
+#include <ftxui/ftxui.hpp>
+#include <string>
+#include <vector>
+using namespace ftxui;
 
 int main() {
-    //ttyshark();
-    /*auto monitor = serialcap("device goes here", B9600, "./cap.bin");
-    monitor.start(0, 2);
-    sleep(15);
-    monitor.stop();
-    if (auto output = monitor.wait()) {
-        printf("Successfully stopped\n");
-    } else {
-        printf("Failed to stop successfully\n");
-        printf("%s\n", output.error().what());
-        return -1;
-    }*/
-    auto start = std::chrono::high_resolution_clock::now();
-    auto addresses = modbusrtu::plausibleTwo("./cap.bin");
-    auto end = std::chrono::high_resolution_clock::now();
+  auto document = vbox({
+      hbox({
+          text("one") | border,
+          text("two") | border | flex,
+          text("three") | border | flex,
+      }),
 
-    auto addr1 = addresses.first;
-    auto addr2 = addresses.second;
+      gauge(0.25) | color(Color::Red),
+      gauge(0.50) | color(Color::White),
+      gauge(0.75) | color(Color::Blue),
+  });
 
-    printf("cap.bin Histogram Guesses\n");
-    printf("Guessed Addr1: %#x\n", addr1);
-    printf("Guessed Addr2: %#x\n", addr2);
+  auto screen = Screen::Create(Dimension::Full());
+  Render(screen, document);
+  screen.Print();
+  // ttyshark();
+  /*auto monitor = serialcap("device goes here", B9600, "./cap.bin");
+  monitor.start(0, 2);
+  sleep(15);
+  monitor.stop();
+  if (auto output = monitor.wait()) {
+      printf("Successfully stopped\n");
+  } else {
+      printf("Failed to stop successfully\n");
+      printf("%s\n", output.error().what());
+      return -1;
+  }
+  auto start = std::chrono::high_resolution_clock::now();
+  auto addresses = modbusrtu::plausibleTwo("./cap.bin");
+  auto end = std::chrono::high_resolution_clock::now();
 
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    printf("Execution took: %lld ms\n", static_cast<long long>(duration.count()));
-    printf("Smart Search\n");
+  auto addr1 = addresses.first;
+  auto addr2 = addresses.second;
 
-    start = std::chrono::high_resolution_clock::now();
-    auto addresses2 = modbusrtu::smartTwo("./cap.bin");
-    end = std::chrono::high_resolution_clock::now();
+  printf("cap.bin Histogram Guesses\n");
+  printf("Guessed Addr1: %#x\n", addr1);
+  printf("Guessed Addr2: %#x\n", addr2);
 
-    addr1 = addresses2.first;
-    addr2 = addresses2.second;
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end -
+  start); printf("Execution took: %lld ms\n", static_cast<long
+  long>(duration.count())); printf("Smart Search\n");
 
-    printf("Guessed Addr1: %#x\n" , addr1);
-    printf("Guessed Addr2: %#x\n" , addr2);
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    printf("Execution took: %lld ms\n", static_cast<long long>(duration.count()));
-    printf("Matrix of Confidence: \n");
-    auto matrix = modbusrtu::confidenceMatrix(addresses, addresses2);
-    for (auto row : matrix) {
-        for (auto val : row) {
-            printf("   %d   ", val);
-        }
-        printf("\n");
-    }
+  start = std::chrono::high_resolution_clock::now();
+  auto addresses2 = modbusrtu::smartTwo("./cap.bin");
+  end = std::chrono::high_resolution_clock::now();
 
-    printf("\n\n\n");
+  addr1 = addresses2.first;
+  addr2 = addresses2.second;
+
+  printf("Guessed Addr1: %#x\n" , addr1);
+  printf("Guessed Addr2: %#x\n" , addr2);
+  duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  printf("Execution took: %lld ms\n", static_cast<long long>(duration.count()));
+  printf("Matrix of Confidence: \n");
+  auto matrix = modbusrtu::confidenceMatrix(addresses, addresses2);
+  for (auto row : matrix) {
+      for (auto val : row) {
+          printf("   %d   ", val);
+      }
+      printf("\n");
+  }
+
+  printf("\n\n\n");
 
 
-    auto results = modbusrtu::lexCapture("./cap.bin");
-    printf("Amount Seen: %lu\n", results.size());
-    std::vector<modbusrtu::stringifyPdu> strung = modbusrtu::stringify(results);
-    for (auto str : strung) {
-        modbusrtu::prettyPrint(str);
-        printf("\n");
-    }
+  auto results = modbusrtu::lexCapture("./cap.bin");
+  printf("Amount Seen: %lu\n", results.size());
+  std::vector<modbusrtu::stringifyPdu> strung = modbusrtu::stringify(results);
+  for (auto str : strung) {
+      modbusrtu::prettyPrint(str);
+      printf("\n");
+  }
 
-    /*start = std::chrono::high_resolution_clock::now();
-    addresses = modbusrtu::plausibleTwo("./cap.bin");
-    end = std::chrono::high_resolution_clock::now();
+  /*start = std::chrono::high_resolution_clock::now();
+  addresses = modbusrtu::plausibleTwo("./cap.bin");
+  end = std::chrono::high_resolution_clock::now();
 
-    addr1 = addresses.first;
-    addr2 = addresses.second;
+  addr1 = addresses.first;
+  addr2 = addresses.second;
 
-    printf("cap.bin Histogram Guesses\n");
-    printf("Guessed Addr1: %#x\n", addr1);
-    printf("Guessed Addr2: %#x\n", addr2);
+  printf("cap.bin Histogram Guesses\n");
+  printf("Guessed Addr1: %#x\n", addr1);
+  printf("Guessed Addr2: %#x\n", addr2);
 
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    printf("Execution took: %lld ms\n", static_cast<long long>(duration.count()));
-    printf("Smart Search\n");
+  duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  printf("Execution took: %lld ms\n", static_cast<long long>(duration.count()));
+  printf("Smart Search\n");
 
-    start = std::chrono::high_resolution_clock::now();
-    addresses2 = modbusrtu::smartTwo("./cap.bin");
-    end = std::chrono::high_resolution_clock::now();
+  start = std::chrono::high_resolution_clock::now();
+  addresses2 = modbusrtu::smartTwo("./cap.bin");
+  end = std::chrono::high_resolution_clock::now();
 
-    addr1 = addresses2.first;
-    addr2 = addresses2.second;
+  addr1 = addresses2.first;
+  addr2 = addresses2.second;
 
-    printf("Guessed Addr1: %#x\n" , addr1);
-    printf("Guessed Addr2: %#x\n" , addr2);
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    printf("Execution took: %lld ms\n", static_cast<long long>(duration.count()));
-    printf("Matrix of Confidence: \n");
-    matrix = modbusrtu::confidenceMatrix(addresses, addresses2);
-    for (auto row : matrix) {
-        for (auto val : row) {
-            printf("   %d   ", val);
-        }
-        printf("\n");
-    }
+  printf("Guessed Addr1: %#x\n" , addr1);
+  printf("Guessed Addr2: %#x\n" , addr2);
+  duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  printf("Execution took: %lld ms\n", static_cast<long long>(duration.count()));
+  printf("Matrix of Confidence: \n");
+  matrix = modbusrtu::confidenceMatrix(addresses, addresses2);
+  for (auto row : matrix) {
+      for (auto val : row) {
+          printf("   %d   ", val);
+      }
+      printf("\n");
+  }
 
-    printf("\n\n\n");
+  printf("\n\n\n");
 
-    start = std::chrono::high_resolution_clock::now();
-    addresses = modbusrtu::plausibleTwo("./cap.bin");
-    end = std::chrono::high_resolution_clock::now();
+  start = std::chrono::high_resolution_clock::now();
+  addresses = modbusrtu::plausibleTwo("./cap.bin");
+  end = std::chrono::high_resolution_clock::now();
 
-    addr1 = addresses.first;
-    addr2 = addresses.second;
+  addr1 = addresses.first;
+  addr2 = addresses.second;
 
-    printf("cap.bin Histogram Guesses\n");
-    printf("Guessed Addr1: %#x\n", addr1);
-    printf("Guessed Addr2: %#x\n", addr2);
+  printf("cap.bin Histogram Guesses\n");
+  printf("Guessed Addr1: %#x\n", addr1);
+  printf("Guessed Addr2: %#x\n", addr2);
 
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    printf("Execution took: %lld ms\n", static_cast<long long>(duration.count()));
-    printf("Smart Search\n");
+  duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  printf("Execution took: %lld ms\n", static_cast<long long>(duration.count()));
+  printf("Smart Search\n");
 
-    start = std::chrono::high_resolution_clock::now();
-    addresses2 = modbusrtu::smartTwo("./cap.bin");
-    end = std::chrono::high_resolution_clock::now();
+  start = std::chrono::high_resolution_clock::now();
+  addresses2 = modbusrtu::smartTwo("./cap.bin");
+  end = std::chrono::high_resolution_clock::now();
 
-    addr1 = addresses2.first;
-    addr2 = addresses2.second;
+  addr1 = addresses2.first;
+  addr2 = addresses2.second;
 
-    printf("Guessed Addr1: %#x\n" , addr1);
-    printf("Guessed Addr2: %#x\n" , addr2);
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    printf("Execution took: %lld ms\n", static_cast<long long>(duration.count()));
-    printf("Matrix of Confidence: \n");
-    matrix = modbusrtu::confidenceMatrix(addresses, addresses2);
-    for (auto row : matrix) {
-        for (auto val : row) {
-            printf("   %d   ", val);
-        }
-        printf("\n");
-    }*/
+  printf("Guessed Addr1: %#x\n" , addr1);
+  printf("Guessed Addr2: %#x\n" , addr2);
+  duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  printf("Execution took: %lld ms\n", static_cast<long long>(duration.count()));
+  printf("Matrix of Confidence: \n");
+  matrix = modbusrtu::confidenceMatrix(addresses, addresses2);
+  for (auto row : matrix) {
+      for (auto val : row) {
+          printf("   %d   ", val);
+      }
+      printf("\n");
+  }*/
 
-    return 1;
+  return 1;
 }
