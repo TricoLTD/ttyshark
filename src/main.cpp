2de60c1f3c4ebc3f09d67aa67ac065711cb7eb97
ViridianAStar <bowman9594@gmail.com>
2026-09-15 13:06:56 -0400

    timeout blocking read options

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,37 @@
 #include "serialcap/serialcap.h"
 #include "ttyshark.h"
 #include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
 #include <stdio.h>
 #include <string>
 #include <vector>
 #define USAGE                                                                  \
   "Usage: ttyshark [--help | -h]\n"                                            \
-  "ttyshark [--capture | -c] <serial_file>\n"
+  "ttyshark [--capture | -c] <serial_device> <capture_location [default is "   \
+  "./cap_Y-M-D-H-M-S.bin]> : capture of a serial device specification is "     \
+  "order sensitive.\n"
+
+void runCap(std::string device, std::string location) {
+  printf("How many seconds to run capture for: ");
+  int waitTime;
+  scanf("%d", &waitTime);
+  printf("What is the baud: ");
+  int bRate;
+  scanf("%d", &bRate);
+  auto cap = serialcap(device, bRate, location);
+  cap.start(waitTime, 0);
+  auto result = cap.wait();
+
+  if (result) {
+    printf("%d", *result);
+  } else {
+    printf("%s", result.error().what());
+  }
+
+  return;
+}
 
 int main(int argc, char *argv[]) {
   // printf("%d", argc);
@@ -19,6 +44,31 @@ int main(int argc, char *argv[]) {
   for (size_t i = 0; i < args.size(); i++) {
     if (args[i] == "-h" || args[i] == "--help") {
       printf("%s", USAGE);
+    } else if (args[i] == "--capture" || args[i] == "-c") {
+      if ((i + 2) < args.size()) {
+        auto sD = static_cast<std::string>(args[i + 1]);
+        auto cL = static_cast<std::string>(args[i + 2]);
+        runCap(sD, cL);
+        i += 2;
+      } else {
+        if ((i + 1) < args.size()) {
+          auto now = std::chrono::system_clock::now();
+          std::time_t nowT = std::chrono::system_clock::to_time_t(now);
+
+          struct tm local_time;
+          localtime_r(&nowT, &local_time);
+
+          std::stringstream nameString;
+          nameString << std::put_time(&local_time,
+                                      "./cap_%Y-%m-%d-%H-%M-%S.bin");
+          std::string fileString = nameString.str();
+          auto sD = static_cast<std::string>(args[i + 1]);
+
+          runCap(sD, fileString);
+        } else {
+          printf("Invalid arguments for capture");
+        }
+      }
     }
   }
   return 1;
